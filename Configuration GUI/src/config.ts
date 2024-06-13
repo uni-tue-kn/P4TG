cc73c31de667a72c654e75e8a0a76c57d071c189
Steffen Lindner <slindner1996@gmail.com>
2024-06-13 14:12:49 +0200

    Update API URL

--- Configuration GUI/src/config.ts
+++ Configuration GUI/src/config.ts
@@ -19,8 +19,8 @@
 
 
 const config = {
-    API_URL: "http://localhost:8082/api",
-    //API_URL: window.location.protocol + "//" + window.location.host + "/api",
+    //API_URL: "http://localhost:8082/api",
+    API_URL: window.location.protocol + "//" + window.location.host + "/api",
     BASE_PATH: "/"
 }
 
