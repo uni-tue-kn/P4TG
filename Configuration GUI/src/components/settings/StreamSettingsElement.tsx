b448fa87181ae6c93028f56a4b51154b9e06b898
Fabian Ihle <fabian.ihle@uni-tuebingen.de>
2024-08-01 23:40:46 +0200

    Automatically disable active stream setting if port goes down

--- Configuration GUI/src/components/settings/StreamSettingsElement.tsx
+++ Configuration GUI/src/components/settings/StreamSettingsElement.tsx
@@ -25,18 +25,19 @@ import {StyledCol} from "../../sites/Settings";
 
 const StreamSettingsElement = ({
                                    running,
+                                   port_status,
                                    stream,
                                    stream_data
-                               }: { running: boolean, stream: StreamSettings, stream_data: Stream }) => {
+                               }: { running: boolean, port_status: boolean, stream: StreamSettings, stream_data: Stream }) => {
     const [show, set_show] = useState(false)
 
     return <>
-        <SettingsModal running={running} data={stream} stream={stream_data} show={show} hide={() => set_show(false)}/>
+        <SettingsModal running={running || !port_status} data={stream} stream={stream_data} show={show} hide={() => set_show(false)}/>
         <StyledCol>
             <Form.Check
                 className={"d-inline"}
-                disabled={running}
-                defaultChecked={stream.active}
+                disabled={running || !port_status}
+                defaultChecked={stream.active && port_status}
                 type={"switch"}
                 onChange={(event) => {
                     stream.active = !stream.active
